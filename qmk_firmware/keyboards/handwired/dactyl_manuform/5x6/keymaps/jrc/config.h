441f5ac84fffaa920116b1b81a0e7a2f088ee59e
Justin Cai <justin.cai101@gmail.com>
2020-02-18 21:40:16 -0800

    update keymap, ctl to thumb

--- qmk_firmware/keyboards/handwired/dactyl_manuform/5x6/keymaps/jrc/config.h
+++ qmk_firmware/keyboards/handwired/dactyl_manuform/5x6/keymaps/jrc/config.h
@@ -20,8 +20,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 #define USE_SERIAL
-
+#define SPLIT_USB_DETECT
+#define SPLIT_USB_TIMEOUT 2500
 #define MASTER_LEFT
 // #define MASTER_RIGHT
-//#define EE_HANDS
+// #define EE_HANDS
 // Rows are doubled-up
