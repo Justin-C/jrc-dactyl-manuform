d91c0dfb83d9334f9d902d5a60a93afd75e52fc7
Justin Cai <justin.cai101@gmail.com>
2020-04-11 18:11:53 -0700

    remove redundant codes, bracket to rhs, knob raise layer, media keys

--- qmk_firmware/keyboards/handwired/dactyl_manuform/5x6/keymaps/jrc/keymap.c
+++ qmk_firmware/keyboards/handwired/dactyl_manuform/5x6/keymaps/jrc/keymap.c
@@ -17,6 +17,7 @@
 static bool rbrc_down = false;
 static bool plus_down = false;
 static bool onMacLayer = false;
+static bool raise_down = false;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -27,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      RAISE, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                           KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
      KC_MUTE,                 KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
                                       KC_LCTL,KC_ENT,                        KC_SPC, LOWER,
-                                      KC_TAB,KC_HOME,                         KC_END,  KC_DEL,
+                                      KC_TAB,_______,                         _______,  KC_DEL,
                                       KC_BSPC, KC_GRV,                        KC_LGUI, KC_LALT,
                                                                                     KC_MS_U, KC_MS_L, KC_BTN1, KC_MS_D, KC_MS_R
 
@@ -40,16 +41,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      RAISE, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                           KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
      KC_MUTE,                 KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
                                       KC_LGUI,KC_ENT,                        KC_SPC, LOWER,
-                                      KC_TAB,KC_HOME,                         KC_LCTL,  KC_DEL,
+                                      KC_TAB,_______,                         _______,  KC_DEL,
                                       KC_BSPC, KC_GRV,                        KC_LGUI, KC_LALT,
-                                           KC_UP, KC_LEFT, KC_ENT, KC_DOWN, KC_RIGHT
+                                                                                    KC_MS_U, KC_MS_L, KC_BTN1, KC_MS_D, KC_MS_R
   ),
   [_LOWER] = LAYOUT_5x6(
 
-     _______,_______,_______,_______,_______,_______,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
-     _______,_______,_______,_______,_______,_______,                        KC_RBRC, _______ , KC_UP , _______ ,_______,KC_PLUS,
-     _______,_______,_______,_______,_______,_______,                        KC_RPRN, KC_LEFT , KC_DOWN , KC_RIGHT ,KC_MINS,KC_PIPE,
-     _______,_______,_______,_______,_______,_______,                        _______, _______ , _______ , _______ ,_______ ,_______,
+     _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
+     _______,_______,_______,_______,_______,_______,                        _______, _______ , KC_UP , _______ ,_______,_______,
+     _______,_______,_______,_______,_______,_______,                        _______, KC_LEFT , KC_DOWN , KC_RIGHT ,_______,_______,
+     _______,_______,_______,_______,_______,_______,                        _______, KC_MPRV , KC_MPLY , KC_MNXT ,_______ ,_______,
     KC_MUTE,                                  KC_NO,KC_PSCR,            _______, _______,
                                              KC_NO,_______,            _______,_______,
                                              _______,_______,            _______,_______,
@@ -60,14 +61,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT_5x6(
        KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
-       _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
-       _______,_______,_______  ,_______,_______,KC_LPRN,                        KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
-       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,KC_VOLD,
+       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______ ,KC_LBRC,KC_RBRC,
+       _______,_______,_______  ,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
+       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
        KC_MUTE,                                _______,_______,            KC_EQL ,_______,
                                                _______,_______,            _______,_______,
                                                _______,_______,            _______,_______,
                                                _______,_______,            _______,_______,
-                                       KC_MS_U, KC_MS_L, KC_BTN1, KC_MS_D, KC_MS_R
+                                                                                         KC_UP, KC_LEFT, KC_ENT, KC_DOWN, KC_RIGHT
   )
 };
 
@@ -75,6 +76,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
 {
     switch (keycode)
     {
+    case RAISE:
+        if (record->event.pressed)
+        {
+            raise_down = true;
+            layer_on(_RAISE);
+        }
+        else
+        {
+            raise_down = false;
+            layer_off(_RAISE);
+
+        }
+        return false;
+        break;
     case KC_RBRC:
         if (record->event.pressed)
         {
@@ -143,10 +158,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
 
 void encoder_update_user(uint8_t index, bool clockwise) {
     if (clockwise) {
-    //   tap_code(KC_PGDN);
-        tap_code(KC_VOLU);
+        if(raise_down){
+            tap_code(KC_PGUP);
+        }else{
+            tap_code(KC_VOLD);
+        }
     } else {
-    //   tap_code(KC_PGUP);
-        tap_code(KC_VOLD);
+        if(raise_down){
+            tap_code(KC_PGDN);
+        }else{
+            tap_code(KC_VOLU);
+        }
     }
 }
